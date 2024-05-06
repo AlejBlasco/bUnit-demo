1af9b7e64a83a0ff8a3d50d635f75a0124d4ac52
Alejandro Blasco <alejandro.blasco@pronet-ise.com>
2024-05-06 10:06:01 +0200

    feat: Initial commit

---
+++ test/FrontWasm.UnitTest/Pages/CounterCSharpIsolatedContextTest.cs
@@ -0,0 +1,36 @@
+﻿using FrontWasm.Pages;
+
+namespace FrontWasm.UnitTest.Pages;
+
+public class CounterCSharpIsolatedContextTest
+{
+    [Fact]
+    public void Counter_should_start_at_zero()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+        var actual = ctx.RenderComponent<Counter>();
+
+        // Act
+
+        // Assert
+        actual.Find("p")
+            .MarkupMatches("<p role=\"status\">Current count: 0</p>");
+    }
+
+    [Fact]
+    public void OnClick_should_increment_number()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+        var actual = ctx.RenderComponent<Counter>();
+
+        // Act
+        actual.Find("button")
+            .Click();
+
+        // Assert
+        actual.Find("p")
+            .MarkupMatches("<p role=\"status\">Current count: 1</p>");
+    }
+}
