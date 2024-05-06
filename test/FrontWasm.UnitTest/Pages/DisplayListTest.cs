714e3bbcfdc57749872c98ddb7973123ff0017fe
Alejandro Blasco <alejandro.blasco@pronet-ise.com>
2024-05-06 10:34:06 +0200

    test: Add type parameters example

---
+++ test/FrontWasm.UnitTest/Pages/DisplayListTest.cs
@@ -0,0 +1,36 @@
+﻿using AngleSharp;
+using FluentAssertions;
+using FrontWasm.Pages;
+using System;
+using System.Collections.Generic;
+
+namespace FrontWasm.UnitTest.Pages;
+
+public class DisplayListTest : TestContext
+{
+    [Fact]
+    public void List_should_get_parameters()
+    {
+        // Arrange
+        var items = new List<string> { "Hello", "World" };
+        var number = new Random().Next(1, 10);
+
+        var actual = RenderComponent<DisplayList>(parameters => parameters
+            .Add(p => p.Number, number)
+            .Add(p => p.Items, items)
+            );
+
+        // Act
+        var numberMarkup = actual.Find("p")
+            .ToMarkup();
+
+        var itemsMarkup = actual.Find("ul").ToHtml();
+
+        // Assert
+        numberMarkup.Should().NotBeNullOrEmpty();
+        numberMarkup.Trim().Should().BeEquivalentTo($"<p>Your number is: {number}</p>");
+
+        itemsMarkup.Should().NotBeNullOrEmpty();
+        itemsMarkup.Trim().Should().BeEquivalentTo("<ul><li>Hello</li><li>World</li></ul>");
+    }
+}
