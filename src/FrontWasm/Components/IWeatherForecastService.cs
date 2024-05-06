6a539e04f295d24dd022944a45413e734c8792f8
Alejandro Blasco <alejandro.blasco@pronet-ise.com>
2024-05-06 11:42:15 +0200

    feat: Add weatherForecast service

---
+++ src/FrontWasm/Components/IWeatherForecastService.cs
@@ -0,0 +1,87 @@
+﻿using System.Text.Json;
+using static FrontWasm.Pages.Weather;
+
+namespace FrontWasm.Components
+{
+    public interface IWeatherForecastService
+    {
+        WeatherForecast[]? GetForecast();
+
+        WeatherForecast[]? GetForecast(DateTime dateTime);
+
+        Task<WeatherForecast[]?> GetForecastAsync();
+
+        Task<WeatherForecast[]?> GetForecastAsync(DateTime dateTime);
+    }
+
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private const string weatherJson = $@"[
+              {{
+                ""date"": ""2022-01-06"",
+                ""temperatureC"": 1,
+                ""summary"": ""Freezing""
+              }},
+              {{
+                ""date"": ""2022-01-07"",
+                ""temperatureC"": 14,
+                ""summary"": ""Bracing""
+              }},
+              {{
+                ""date"": ""2022-01-08"",
+                ""temperatureC"": -13,
+                ""summary"": ""Freezing""
+              }},
+              {{
+                ""date"": ""2022-01-09"",
+                ""temperatureC"": -16,
+                ""summary"": ""Balmy""
+              }},
+              {{
+                ""date"": ""2022-01-10"",
+                ""temperatureC"": -2,
+                ""summary"": ""Chilly""
+              }}
+            ]
+            ";
+        private readonly WeatherForecast[]? forecasts;
+
+        public WeatherForecastService()
+        {
+            forecasts = JsonSerializer.Deserialize<WeatherForecast[]>(weatherJson);
+        }
+
+        public WeatherForecast[]? GetForecast() => forecasts;
+
+        public WeatherForecast[]? GetForecast(DateTime dateTime)
+        {
+            return forecasts?.Where(x => x.Equals(dateTime))
+                .ToArray();
+        }
+
+        public async Task<WeatherForecast[]?> GetForecastAsync()
+        {
+            WeatherForecast[]? result = null;
+
+            await Task.Run(() =>
+            {
+                result = GetForecast();
+            });
+
+            return result;
+        }
+
+        public async Task<WeatherForecast[]?> GetForecastAsync(DateTime dateTime)
+        {
+            WeatherForecast[]? result = null;
+
+            await Task.Run(() =>
+            {
+                result = GetForecast(dateTime);
+            });
+
+            return result;
+        }
+
+    }
+}
