cf3d2da9fd36f456219116cd6a7d5e4966cf6a4e
Alejandro Blasco <alejandro.blasco@pronet-ise.com>
2024-05-08 10:26:58 +0200

    test: Add Trigger renders tests

---
+++ test/FrontWasm.UnitTest/Pages/Interaction/TriggeringCustomEventsTest.cs
@@ -0,0 +1,39 @@
+﻿using FrontWasm.Pages.Interaction;
+using System;
+using static FrontWasm.Pages.Interaction.TriggeringCustomEvents;
+
+namespace FrontWasm.UnitTest.Pages.Interaction;
+
+public class TriggeringCustomEventsTest : TestContext
+{
+    public void CustomEvent_should_change_text()
+    {
+        // Arrange
+        var actual = RenderComponent<TriggeringCustomEvents>();
+
+        // Act
+        actual.Find("input")
+            .TriggerEvent("oncustompaste", new CustomPasteEventArgs
+            {
+                EventTimestamp = DateTime.Now,
+                PastedData = "HELLO WORLD"
+            });
+
+        // Assert
+        actual.Find("p").MarkupMatches("<p>You pasted: HELLO WORLD</p>");
+    }
+
+    [Fact]
+    public void ClickEvent_should_reset_text()
+    {
+        // Arrange
+        var actual = RenderComponent<TriggeringCustomEvents>();
+
+        // Act
+        actual.Find("button")
+            .Click();
+
+        // Assert
+        actual.Find("p").MarkupMatches("<p></p>");
+    }
+}
---
+++ test/FrontWasm.UnitTest/Pages/Interaction/TriggeringRendersTest.cs
@@ -0,0 +1,108 @@
+﻿using FluentAssertions;
+using FrontWasm.Pages.Interaction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontWasm.UnitTest.Pages.Interaction;
+
+public class TriggeringRendersTest : TestContext
+{
+    [Fact]
+    public void Render_should_increment_render_count()
+    {
+        // Arrange
+        var actual = RenderComponent<TriggeringRenders>();
+
+        // Act
+        int initCount = actual.RenderCount;
+        actual.Render();
+
+        // Assert
+        actual.Should().NotBeNull();
+
+        initCount.Should().Be(1);
+        actual.RenderCount.Should().Be(2);
+    }
+
+    [Fact]
+    public void Render_should_set_new_params_value()
+    {
+        // Arrange
+        var actual = RenderComponent<TriggeringRenders>(parameters => parameters
+          .Add(p => p.Value, "Foo")
+        );
+
+        // Act
+        var initMarkup = actual.Markup;
+        actual.SetParametersAndRender(parameters => parameters
+          .Add(p => p.Value, "Bar")
+        );
+
+        // Assert
+        actual.Should().NotBeNull();
+
+        initMarkup.Should().BeEquivalentTo("<span>Foo</span>");
+        actual.Markup.Should().BeEquivalentTo("<span>Bar</span>");
+    }
+
+    [Fact]
+    public void InvokeAsync_should_work()
+    {
+        // Arrange
+        var actual = RenderComponent<Calc>();
+
+        // Act
+        actual.InvokeAsync(() => actual.Instance.Calculate(1, 2));
+
+        // Assert
+        actual.Should().NotBeNull();
+        actual.Markup.Should().BeEquivalentTo("<output>3</output>");
+    }
+
+    [Fact]
+    public async Task InvokeAsync_should_work_and_return_value()
+    {
+        // Arrange
+        var actual = RenderComponent<Calc>();
+
+        // Act
+        var result = await actual.InvokeAsync(() => actual.Instance.CalculateAndReturn(1, 2));
+
+
+        // Assert
+        actual.Should().NotBeNull();
+        actual.Markup.Should().BeEquivalentTo("<output>3</output>");
+
+        result.Should().Be(3);
+    }
+
+    [Fact]
+    public async Task InvokeAsync_should_gets_loading()
+    {
+        // Arrange
+        var actual = RenderComponent<CalcWithLoading>();
+
+        // Act
+        await actual.InvokeAsync<Task>(() => actual.Instance.Calculate(1, 2));
+
+        // Assert
+        actual.Markup.Should().BeEquivalentTo("<output>Loading</output>");
+    }
+
+    [Fact]
+    public async Task InvokeAsync_should_gets_result_on_complete_task()
+    {
+        // Arrange
+        var actual = RenderComponent<CalcWithLoading>();
+
+        // Act
+        var task = await actual.InvokeAsync<Task>(() => actual.Instance.Calculate(1, 2));
+        await task;
+
+        // Assert
+        actual.Markup.Should().BeEquivalentTo("<output>3</output>");
+    }
+}
